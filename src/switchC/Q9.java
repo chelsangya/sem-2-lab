7c40c0f9c85e02d5f57d304043e14e791a1543dd
chelsangya <csangya@gmail.com>
2022-04-20 23:53:03 +0545

    Conditional

--- src/switchC/Q9.java
+++ src/switchC/Q9.java
@@ -6,10 +6,21 @@ public class Q9 {
     public static void main(String[] args) {
         Scanner demo= new Scanner(System.in);
         System.out.println("Enter the character ");
+        int x;
         char char1 ;
         char1= demo.next().charAt(0);
-        switch (char1){
-
+        if (Character.isUpperCase(char1)){
+            x=1;
+        } else {
+            x=2;
+        }
+        switch (x){
+            case 1:
+                System.out.println("The character is in Uppercase");
+                break;
+            default:
+                System.out.println("The character is in Lowercase");
+                break;
         }
     }
 }
